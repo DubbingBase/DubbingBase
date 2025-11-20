1d7b9e0b528a8fe46b10a22cf8a022ee0b0bd79c
Quentin Goinaud <quentin.goinaud@gmail.com>
2025-11-20 17:56:18 +0100

    -

--- apps/mobile/src/api/supabase.ts
+++ apps/mobile/src/api/supabase.ts
@@ -1,18 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "@app/supabase/functions/_shared/database.types";
-import { fetch } from "@tauri-apps/plugin-http";
 import { isTauri } from "@/utils/tauri";
-
-import { debug, error, info, trace, warn } from "@tauri-apps/plugin-log";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-if (isTauri) {
-    info("meta:" + JSON.stringify(import.meta));
-    info("supabaseUrl:" + supabaseUrl);
-    info("isTauri:" + isTauri);
-}
 
 const createBaseClient = (user?: string | null) => {
     return createClient<Database>(supabaseUrl, supabaseAnonKey, {
--- apps/mobile/src/utils/tauri.ts
+++ apps/mobile/src/utils/tauri.ts
@@ -1,5 +1,1 @@
-export const isTauri = window.isTauri
-
-console.log('window.__TAURI__', window.__TAURI__)
-console.log('window.__TAURI_INTERNALS__', window.__TAURI_INTERNALS__)
-console.log('window.isTauri', window.isTauri)
+export const isTauri = window.isTauri;
